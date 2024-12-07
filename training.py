cb3d0860976897da519c4b3cbb93aacd032a21b2
geocrew3@gmail.com <geocrew3@gmail.com>
2024-12-07 00:54:38 -0500

    so much change

--- training.py
+++ training.py
@@ -3,12 +3,21 @@ from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
 from models import LSTMModel
+import pickle
 
 model = SVC(kernel = 'rbf', C = 1)
 
+class Trainer():
+    def __init__(self,model):
+        self.model = model
+    
+    def save_model(self, location):
+        with open(f'{location}.pkl', 'wb') as f:
+            pickle.dump(self.model, f)
 
-class SVMTrainer():
+class SVMTrainer(Trainer):
     def __init__(self, model:SVC, kernel='rbf', C=1.0):
+        super().__init__(model)
         self.model = model
         self.model.set_params(kernel = kernel, C = C)
 
@@ -16,28 +25,28 @@ class SVMTrainer():
         self.model.fit(X_train, y_train)
         return self.model
     
-    def validate(self, X_val, y_val):
-        y_pred = self.model.predict(X_val)
-
-        return accuracy_score(y_val, y_pred)
     
-class KNNTrainer():
+class KNNTrainer(Trainer):
     def __init__(self, model:KNeighborsClassifier, n_neighbours, weights):
+        super().__init__(model)
         self.model = model
         self.model.set_params(n_neighbours = n_neighbours, weights = weights)
 
     def train(self, X_train, y_train):
         self.model.fit(X_train, y_train)
         return self.model
-    
-    def validate(self, X_val, y_val):
-        y_pred = self.model.predict(X_val)
-        return accuracy_score(y_val, y_pred) 
-    
-class LSTMTrainer():
-    def __init__(self, model:LSTMModel) -> None:
-        self.model = model
 
-    def train():
+    
+class LSTMTrainer(Trainer):
+    def __init__(self, model:LSTMModel, epochs=15, batch_size = 32) -> None:
+        super().__init__(model)
+        self.model = model
+        self.epochs = epochs
+        self.batch_size = batch_size
+
+    def train(self, X_train, y_train):
         self.model.build_model()
-        
+        self.model.train_model(X_train, y_train, batch_size = self.batch_size, epochs = self.epochs)
+    
+    def save_model(self, location):
+        self.model.export(location)
